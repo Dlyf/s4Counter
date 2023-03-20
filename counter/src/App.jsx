3e277eab840647dde037e4d2ca35b8d0072caa5a
Dlyf <domilung@gmail.com>
2023-03-20 14:57:51 +0100

    createSlice button incremented

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -3,19 +3,27 @@ import { increment } from './store/valueSlice';
 import './App.css'
 
 function App() {
-  const stateDuStore = useSelector((state) => state);
-  console.log("stateDuStore", stateDuStore)
+  // const stateDuStore = useSelector((state) => state);
+  // console.log("stateDuStore", stateDuStore)
+  // const dispatch = useDispatch();
+  // const { number } = useSelector( state => state );
   const dispatch = useDispatch();
+  const { number, parity } = useSelector( state => state );
 
   return (
     <div className="App">
-    
+
       <h1>Counter</h1>
-      <div className="card">
-        <button onClick={() => dispatch(increment()) }>
+      {/* <div className="card">
+        <button onClick={() => dispatch(increment(2)) }>
           increment
         </button> <br />
-          {stateDuStore.e.counter}
+        <p>{number}</p>
+      </div> */}
+       <div className="card">
+        <p>{number}</p>
+        <p>{parity}</p>
+        <button onClick={() => dispatch(increment(2))}>INCREMENT</button>
       </div>
     </div>
   )
--- counter/src/store/valueSlice.jsx
+++ counter/src/store/valueSlice.jsx
@@ -8,28 +8,72 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 // définit un state 
-const initialState = { counter: 0 }
+const aleaMax = 10;
+// const initialState = { number: 0 }
+const initialState = {
+  number: 0,
+  parity: null,
+  step: 0
+}
+const parityName = {
+  even : "even",
+  odd: "odd"
+}
 
-const counterSlice = createSlice({
+const valueSlice = createSlice({
     // clé permettant d'identifier le reducer spécifique 
       name: 'counter',
       initialState,
       // gestions des actions dans le/les reducer(s) du state
+      // reducers: {
+      //   increment: {
+      //     prepare: (coeff) => {
+      //       const alea = Math.floor(Math.random() * aleaMax * coeff) + 1 ;
+      //      return { payload : alea }
+      //     },
+      //     reducer: (state, action) => {
+      //       const  { number } = action.payload;
+      //       state.number += number;
+      //     }
+      //   }
+      // },
       reducers: {
-        increment(state, action) {
-          state.counter = Math.floor(Math.random() * 10);
+        increment:{
+            // le reducer modifie le state
+            reducer: (state, action) => {
+               const { number } =  action.payload;
+               console.log("actioncreateslice", action);
+
+               state.number += number;
+               state.parity = ( state.number % 2 ) ? parityName.odd : parityName.even
+               state.step = number; 
+            },
+            // prépare le payload petite couche avant le reducer
+            prepare: (coeff) => {
+                // valeur aléatoire 
+                const alea = Math.floor(Math.random() * aleaMax * coeff) + 1 ;
+                console.log("paramter of prepare que l'on retrouve dans la méthode incremente dans le component", coeff)
+
+                return {
+                    payload:{
+                        number: alea
+                    }
+                }
+            }
         }
-      },
+    }
     })
 
     // Export des actions
-    export const store = configureStore({
-         reducer: {
-           e : counterSlice.reducer
-        }
-    });
+    // export const store = configureStore({
+    //      reducer: {
+    //        e : counterSlice.reducer
+    //     }
+    // });
 
-    export const { increment } = counterSlice.actions
+    export const { increment } = valueSlice.actions
 
     // pour counterSlice le store dans l'arbre React
-    export default counterSlice.reducer;
+    export default configureStore({
+      reducer: valueSlice.reducer // passe le state pour lecture dans useSelector
+  });
